05cb7ac730bb840a7e362ca360ad57686057f00d
tylerglenski <tglenski1@gmail.com>
2019-08-23 09:58:43 -0500

    Updates

---
+++ precache-manifest.5359d041d834d41638ae61f03ba7307a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48510ab2ada6768b1b640d5a45ff7a3",
+    "url": "/business_card/index.html"
+  },
+  {
+    "revision": "a628f9ed9394916c6cd0",
+    "url": "/business_card/static/css/main.69e66e2b.chunk.css"
+  },
+  {
+    "revision": "9468c7f7a7a41c3ed46d",
+    "url": "/business_card/static/js/2.b649b33a.chunk.js"
+  },
+  {
+    "revision": "a628f9ed9394916c6cd0",
+    "url": "/business_card/static/js/main.8eb7eed9.chunk.js"
+  },
+  {
+    "revision": "cac8bb8eec78855e746e",
+    "url": "/business_card/static/js/runtime~main.dbf6bdaa.js"
+  },
+  {
+    "revision": "664e00e7dd349332d15b1a4052dfe142",
+    "url": "/business_card/static/media/avatar.664e00e7.jpg"
+  },
+  {
+    "revision": "8f9caefb4c85b2bb194f265fb611eaa5",
+    "url": "/business_card/static/media/email.8f9caefb.svg"
+  },
+  {
+    "revision": "1c6ca9ca6bc9d358c3b08c1b9fdc6bcd",
+    "url": "/business_card/static/media/shoppingcart.1c6ca9ca.svg"
+  }
+]);
